87d579e7c9d9cbf69745581dea254d4c6f492999
Hai Pham Ngoc <ngochai285nd@gmail.com>
2024-12-06 00:36:11 +0700

    test: release version v1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="closed-caption",
-    version="1.0.0",
+    version="1.0.1",
     description="Tạo phụ đề từ video sử dụng OpenAI Whisper.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
